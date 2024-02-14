b8b08420715895ae4c98927cc6a9fb8935e52316
Deny <denylson.feitoza@alunos.ufersa.edu.br>
2024-02-14 17:03:14 -0300

    Add Evento model to eventos app

--- eventos/models.py
+++ eventos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Evento(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    local = models.CharField(max_length=100)
+    data = models.DateField()
+    hora = models.TimeField()
+    categoria = models.CharField(max_length=100)
+    banner = models.ImageField(upload_to='eventos', null=True, blank=True)
 
+    def __str__(self) -> str:
+        return self.nome

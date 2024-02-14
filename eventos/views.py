13c954060be2ee285d379ad4438c5a66ec2767bb
Deny <denylson.feitoza@alunos.ufersa.edu.br>
2024-02-14 16:44:16 -0300

    Update forms

--- eventos/views.py
+++ eventos/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def eventos(request):
-    return render(request, 'eventos.html')
+    if request.method == 'GET':
+        return render(request, 'eventos.html')
+    elif request.method == 'POST':
+        nome = request.POST.get('name')
+        descricao = request.POST.get('discription')
+        local = request.POST.get('local')
+        data = request.POST.get('date')
+        hora = request.POST.get('time')
+        categoria = request.POST.get('category')
+
+        return HttpResponse('Evento cadastrado com sucesso!')
---
+++ templates/static/eventos/js/eventos.js
@@ -0,0 +1,4 @@
+function criar_evento(){
+    form = document.getElementById('form-criar-evento');
+    form.submit();
+}

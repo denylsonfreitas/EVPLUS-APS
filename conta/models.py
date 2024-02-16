5117bfb1c11300706da41900d8f75511559eb189
Deny <denylson.feitoza@alunos.ufersa.edu.br>
2024-02-16 11:55:23 -0300

    Add Conta model and update conta.html template

--- conta/models.py
+++ conta/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Conta(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    date = models.DateField()
+    
+    def __str__(self) -> str:
+        return self.nome

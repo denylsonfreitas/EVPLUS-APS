ace11b0abecc8cbb9362470c8d9f3e042d7ad2ea
Deny <denylson.feitoza@alunos.ufersa.edu.br>
2024-04-02 02:28:52 -0300

    Update models.py to add Eventos model

--- meuseventos/models.py
+++ meuseventos/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from eventos.models import Evento as EventoBase
 
-class Evento(EventoBase):
-    class Meta:
-        proxy = True
+class Eventos(models.Model):
+    nome = models.CharField(max_length=100)
+    data = models.DateTimeField(auto_now_add=True)
+    horario = models.TimeField()
 
     def __str__(self) -> str:
         return self.nome

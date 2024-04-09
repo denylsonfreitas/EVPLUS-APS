5c7502a3aec2230be03f37ecd9c418e67ec8f3bc
Deny <denylson.feitoza@alunos.ufersa.edu.br>
2024-04-08 22:42:05 -0300

    Update Conta model and templates to display alert messages in conta.html template

--- conta/views.py
+++ conta/views.py
@@ -1,19 +1,38 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from .models import Conta
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 @login_required(login_url='/auth/login/')
 def conta(request):
     exibir_sidebar = True
     usuario = request.user
+    
     if request.method == 'GET':
         return render(request, 'conta.html', {'exibir_sidebar': exibir_sidebar, 'usuario': usuario})
+    
     elif request.method == 'POST':
-        usuario.first_name = request.POST.get('name')
-        usuario.username = request.POST.get('username')
-        usuario.email = request.POST.get('email')
+        new_name = request.POST.get('name')
+        new_username = request.POST.get('username')
+        new_email = request.POST.get('email')
+        
+        # Verifica se o novo nome de usuário já está cadastrado
+        if new_username != usuario.username and User.objects.filter(username=new_username).exists():
+            messages.error(request, 'O nome de usuário já está em uso. Escolha outro.')
+            return render(request, 'conta.html', {'exibir_sidebar': exibir_sidebar, 'usuario': usuario})
+        
+        # Verifica se o novo email já está cadastrado
+        if new_email != usuario.email and User.objects.filter(email=new_email).exists():
+            messages.error(request, 'O email já está em uso. Escolha outro.')
+            return render(request, 'conta.html', {'exibir_sidebar': exibir_sidebar, 'usuario': usuario})
+
+        usuario.first_name = new_name
+        usuario.username = new_username
+        usuario.email = new_email
         usuario.save()
-        return redirect('home')
+        
+        messages.success(request, 'Alterações salvas com sucesso!')
+        return  render(request, 'conta.html', {'exibir_sidebar': exibir_sidebar, 'usuario': usuario})
 
 @login_required(login_url='/auth/login/')
 def certificado(request):
--- templates/static/conta/js/conta.js
+++ templates/static/conta/js/conta.js
@@ -6,3 +6,10 @@ function salvar_perfil(){
 function exibirPopup() {
     alert("Conta alterada com sucesso!");
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        var alertContainer = document.getElementById('alert-container');
+        alertContainer.innerHTML = '';
+    }, 7000);
+});
